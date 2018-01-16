ba381808b82623d8f724d4dfc8a9f2e2d9a1b1ad
tsne <tsne.dev@outlook.com>
2018-01-16 07:59:12 +0100

    update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,7 @@ const pkg = require('./package.json');
 export default {
 	input: "src/index.js",
 	output: [
-		{file: pkg["module"], format: "es"},
-		{file: pkg["main"], format: "cjs"},
+		{file: pkg["module"], format: "es", sourcemap: true},
+		{file: pkg["main"], format: "cjs", sourcemap: true},
 	],
-	sourcemap: true,
 };
